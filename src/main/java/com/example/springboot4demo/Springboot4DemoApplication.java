6b94e193daf66f87bf4f15d1ef0fb5665e088c93
Ji Mi Lei <22087270+sneakymanatee@users.noreply.github.com>
2025-11-01 01:37:30 +0100

    Initial

---
+++ src/main/java/com/example/springboot4demo/Springboot4DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot4DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot4demo/Springboot4DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot4demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
